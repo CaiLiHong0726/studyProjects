bfbaa441e016133949a6408423112db7f7d75922
clh <519044414@qq,com>
2023-11-22 01:04:01 +0800

    update

---
+++ src/main/java/com/hong/foundation/SPImechanism/DatabaseSearch.java
@@ -0,0 +1,12 @@
+package com.hong.foundation.SPImechanism;
+
+import java.util.List;
+
+public class DatabaseSearch  implements Search{
+    @Override
+    public List<String> searchDoc(String keyword) {
+        System.out.println("数据搜索 "+keyword);
+        return null;
+    }
+}
+
---
+++ src/main/java/com/hong/foundation/SPImechanism/Search.java
@@ -0,0 +1,7 @@
+package com.hong.foundation.SPImechanism;
+
+import java.util.List;
+
+public interface Search {
+    public List<String> searchDoc(String keyword);
+}
